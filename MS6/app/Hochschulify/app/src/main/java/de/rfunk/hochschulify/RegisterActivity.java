0f6f0b7257ebd67b4f62d15ae00bab4126b81723
plsr <christianpoplawski@gmail.com>
2015-11-26 21:39:12 +0100

    Back Button in Register Activity Action Bar

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/RegisterActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/RegisterActivity.java
@@ -9,5 +9,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

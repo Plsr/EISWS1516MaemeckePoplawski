a5a11810355ee5cbdfbf7a47a6788aac7da0db19
plsr <christianpoplawski@gmail.com>
2015-11-26 21:31:24 +0100

    Implemented Transition to Register Activity
    Transition is triggered when th user activates the 'Registrieren'-Button.

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/StartupActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/StartupActivity.java
@@ -1,7 +1,9 @@
 package de.rfunk.hochschulify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+    }
+
+    public void startRegister(View view) {
+        //TODO: In emulator this causes a skip of 100-300 Frames, check behaviour on real device
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }

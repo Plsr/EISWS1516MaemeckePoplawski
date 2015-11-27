58e091a589f4b8096f89aa66550419447c64ec78
plsr <christianpoplawski@gmail.com>
2015-11-27 11:38:38 +0100

    Fixed Toolbar for Register Activity
    This has the padding issue as well, see 7b734551cb01533bd558d352aa0740cc2481734f

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/RegisterActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/RegisterActivity.java
@@ -2,16 +2,20 @@ package de.rfunk.hochschulify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //TODO: Add Toolbar
-        //getSupportActionBar().setHomeButtonEnabled(true);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

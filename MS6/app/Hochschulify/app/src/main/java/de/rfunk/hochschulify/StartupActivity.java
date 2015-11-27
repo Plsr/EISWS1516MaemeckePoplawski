475ceaff13b03cfe9aa2d33aae505e62ccfc72d1
plsr <christianpoplawski@gmail.com>
2015-11-27 08:40:09 +0100

    Dummy functionality for Login Process
    You can enter everything you want in the email and password fields, you can even leave it empty and it will work.
    To trigger the error labels enter "falsch" in either of those (or both) inputs.

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/StartupActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/StartupActivity.java
@@ -1,11 +1,17 @@
 package de.rfunk.hochschulify;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.EditText;
 
 public class StartupActivity extends AppCompatActivity {
+
+    EditText identification;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,34 @@ public class StartupActivity extends AppCompatActivity {
     public void startRegister(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Checks the input fields of the login for correctness.
+     * For now, it just checks if one of the values equals "falsch" and displays the error label then,
+     * for demonstration purposes.
+     *
+     * @param view current view
+     */
+    public void performLogin(View view) {
+        // Get the layouts to display the material error labels
+        TextInputLayout identificationLayout = (TextInputLayout)findViewById(R.id.loginIdentification_input_layout);
+        TextInputLayout passwordLayout = (TextInputLayout)findViewById(R.id.loginPassword_input_layout);
+
+        identification = (EditText)findViewById(R.id.loginIdentification);
+        password = (EditText)findViewById(R.id.loginPassword);
+
+        if(identification.getText().toString().equals("falsch"))  {
+            identificationLayout.setError(getString(R.string.err_msg_identification));
+        }
+        else if (password.getText().toString().equals("falsch")) {
+                passwordLayout.setError(getString(R.string.err_msg_password));
+        }
+        else {
+            //TODO: Go to home view
+            System.out.println("Yo");
+        }
+
+
     }
 }

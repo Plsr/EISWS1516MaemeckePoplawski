a41b364b8c0869cc593c816dbfe5a5e9d07e8f87
plsr <christianpoplawski@gmail.com>
2015-11-29 19:38:02 +0100

    Basic ListView for Notifications

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/NotificationsFragment.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/NotificationsFragment.java
@@ -6,6 +6,11 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,6 +19,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class NotificationsFragment extends android.support.v4.app.Fragment {
+
+    private ListView listView;
+    private static String NOTIFICATION_TEMPLATE = "Neuer Kommentar zu ";
 
     public NotificationsFragment() {
         // Required empty public constructor
@@ -30,6 +38,24 @@ public class NotificationsFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        listView = (ListView)getView().findViewById(R.id.listView);
+        List<String> listContent = new ArrayList<>();
+        listContent.add("nrw.de");
+        listContent.add("dekra-hochschule-berlin");
+        listContent.add("Medieninformatik");
+        listContent.add("businessschool-berlin-potsdam");
+        listContent.add("BWL");
+        listContent.add("Deutsch TH Köln");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, listContent);
+        listView.setAdapter(arrayAdapter);
+    }
+
 
 
 }

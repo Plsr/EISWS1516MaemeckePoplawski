8c21513577010307e778212c2257119ae9d87820
plsr <christianpoplawski@gmail.com>
2015-11-30 17:14:35 +0100

    Profile mostly done

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/ProfileEditActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/ProfileEditActivity.java
@@ -1,30 +1,35 @@
 package de.rfunk.hochschulify;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class ProfileActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
+public class ProfileEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_edit);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Profil");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Profil bearbeiten");
         setSupportActionBar(toolbar);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.profile_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.profile_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        getMenuInflater().inflate(R.menu.menu_save, menu);
         return true;
     }
 

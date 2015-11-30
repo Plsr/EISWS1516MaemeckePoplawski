74a0e568469b48c9e0e860d8a183e5da19c1fdfb
plsr <christianpoplawski@gmail.com>
2015-11-30 13:24:56 +0100

    Basic layout for written Threads view

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/WrittenThreadsActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/WrittenThreadsActivity.java
@@ -2,12 +2,18 @@ package de.rfunk.hochschulify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class WrittenThreadsActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiritten_threads);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Meine Threads");
+        setSupportActionBar(toolbar);
     }
 }

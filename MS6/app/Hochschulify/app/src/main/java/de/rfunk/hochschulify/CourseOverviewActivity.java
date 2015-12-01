3908bc03c8d63b18ce7d005fcf7250878847831c
plsr <christianpoplawski@gmail.com>
2015-12-01 13:12:32 +0100

    Acturally implemented ability to swap to next activity

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/CourseOverviewActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/CourseOverviewActivity.java
@@ -1,5 +1,6 @@
 package de.rfunk.hochschulify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,7 +21,8 @@ public class CourseOverviewActivity extends AppCompatActivity {
         findViewById(R.id.card1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Yay?");
+                Intent intent = new Intent(CourseOverviewActivity.this, SingleThreadActivity.class);
+                startActivity(intent);
             }
         });
     }

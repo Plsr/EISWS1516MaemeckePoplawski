8ed634ef28afb042ea724defa843b10d77fb6f2d
plsr <christianpoplawski@gmail.com>
2015-12-01 09:59:38 +0100

    Search 'functionality' moved to activity

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SearchActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SearchActivity.java
@@ -2,12 +2,37 @@ package de.rfunk.hochschulify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ListView listView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Suche");
+        setSupportActionBar(toolbar);
+
+        listView = (ListView)findViewById(R.id.listView);
+        List<String> listContent = new ArrayList<>();
+        listContent.add("nrw.de");
+        listContent.add("dekra-hochschule-berlin");
+        listContent.add("Medieninformatik");
+        listContent.add("businessschool-berlin-potsdam");
+        listContent.add("BWL");
+        listContent.add("Deutsch TH Köln");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listContent);
+        listView.setAdapter(arrayAdapter);
     }
 }

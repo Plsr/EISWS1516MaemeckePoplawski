c2b35b14b2579384b5e98f892ea1b94186471819
plsr <christianpoplawski@gmail.com>
2015-12-01 13:52:01 +0100

    Implemented Thread Single View
    We'll need a ScrollView here later (as well as in many other places) to prevent the cards from being cut, but it'd be too much work (regarding the time we have left) to do that now.

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SingleThreadActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SingleThreadActivity.java
@@ -2,6 +2,7 @@ package de.rfunk.hochschulify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SingleThreadActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SingleThreadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_thread);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Ein Thread");
+        setSupportActionBar(toolbar);
     }
 }

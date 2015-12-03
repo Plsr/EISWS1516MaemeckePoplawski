839d9a0029967514c51da19b25c7b9559657c04c
plsr <christianpoplawski@gmail.com>
2015-12-03 13:04:20 +0100

    Most Client side Login functionality
    Client now stores the login credentials in the Shared Preferences if all inputs contain data.
    It's not longer possible to leave the input empty an click Login to move forward to the home view, but it still doesn't matter what values are entered in the input field.
    Note that the input of "falsch" will no longer force the error message to appear.
    
    Also implemented Utils.java, a class with handy helper methods (for now mostly helpers regarding Shared Preferences).

---
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/Utils.java
@@ -0,0 +1,98 @@
+package de.rfunk.hochschulify;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Class with general helper methods.
+ *
+ * Created by chrispop on 03/12/15.
+ */
+public class Utils {
+
+    //TODO: Remove Password, only store login token
+    public static final String LOGIN_USERNAME_KEY = "__USERNAME__";
+    public static final String LOGIN_PASSWORD_KEY = "__PASSWORD__";
+
+
+    /**
+     * Saves a key-value-pair to the default shared preferences.
+     *
+     * Although there shouldn't be sent any empty strings to the function, it checks for it again.
+     *
+     * This function was taken from Stack Overflow and slightly improved
+     * @see <a href="http://stackoverflow.com/a/19629701/4181679">Stack Overflow Thread</a>
+     *
+     * @param context Context of the calling method
+     * @param key The key
+     * @param value The Value
+     * @return true if save was successful, false otherwise
+     */
+    public static boolean saveToSharedPrefs (Context context, String key, String value) {
+        if(stringNotEmpty(key) && stringNotEmpty(value)) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+            final SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putString(key, value);
+            editor.apply();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Return the value of a given key from the default shared memory.
+     *
+     * The defaultValue is specified by the caller to make checking against it mor convenient.
+     *
+     * NOTE: Only returns the defaultValue if the key does not exist.
+     * If it exists but no value is set, the returned string is empty.
+     *
+     * This function was taken from Stack Overflow
+     * @see <a href="http://stackoverflow.com/a/19629701/4181679">Stack Overflow Thread</a>
+     *
+     * @param context Context of the calling method
+     * @param key The key
+     * @param defaultValue defaultValue specified by the caller
+     * @return The value matching the key or the defaultValue
+     */
+    public static String getFromSharedPrefs (Context context, String key, String defaultValue) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        try {
+            return sharedPrefs.getString(key, defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Clears all keys and values from default shared preferences
+     *
+     * NOTE: This method was written for development and testing purposes only. Never use this
+     * method in production.
+     * Always remove single key & value pairs from the shared memory to keep control over what is deleted.
+     *
+     * @param context Context of the calling method
+     */
+    public static void clearSharedPreferences(Context context) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    /**
+     * Helper method to test if a given string is empty
+     *
+     * This methods main purpose is to save space and improve readability.
+     * Checks if the given String is null and checks it with the .isEmpty() function.
+     *
+     * @param string The String to test
+     * @return true if String is not empty
+     */
+    public static boolean stringNotEmpty(String string) {
+        return string != null && !string.isEmpty();
+    }
+}

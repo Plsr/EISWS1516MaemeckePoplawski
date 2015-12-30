927025f2993eda12490ecad87d3144b0a68f5181
plsr <christianpoplawski@gmail.com>
2015-12-30 11:48:12 +0100

    Basic GET request for Entries implemented
    Still very Basic, Thread ID is static, none of the returned values is actually rendered, but the communication with the Server and the Entries route is working.

--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SingleThreadActivity.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/SingleThreadActivity.java
@@ -6,12 +6,33 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SingleThreadActivity extends AppCompatActivity {
+
+    public static final String THREAD_ID = "5683b16de86eec0932e37fac";
+
+    public static final String SERVER_URL = Utils.SERVER_URL;
+    public static final String ENTRY_PATH = Utils.ENTRY_PATH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_thread);
+
+        try {
+            getMainEntry(THREAD_ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Ein Thread");
@@ -24,5 +45,31 @@ public class SingleThreadActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void getMainEntry(String entryID) throws JSONException {
+        JSONObject reqBody = new JSONObject();
+        String url = SERVER_URL + ENTRY_PATH + "/" + entryID;
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, reqBody, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String title = response.get("title").toString();
+                    System.out.println(title);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // Add Request to queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(req);
     }
 }
--- MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/Utils.java
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/Utils.java
@@ -15,6 +15,7 @@ public class Utils {
     // Server URL, stored here for global usage
     public static final String SERVER_URL = "http://10.0.2.2:3000";
     public static final String AUTH_PATH = "/auth";
+    public static final String ENTRY_PATH = "/entries";
 
     public static final String USERID_IDENTIFIER = "userid";
     public static final String PASSWORD_IDENTIFIER = "password";

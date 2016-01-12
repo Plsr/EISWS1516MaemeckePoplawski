33ac819defd38fa1e87ce429605a1f85da51198b
Timo Mämecke <timo@maemecke.com>
2016-01-12 13:15:24 +0100

    Entry Class

---
+++ MS6/app/Hochschulify/app/src/main/java/de/rfunk/hochschulify/Entry.java
@@ -0,0 +1,50 @@
+package de.rfunk.hochschulify;
+
+/**
+ * Created by Timo Maemecke (@timomeh) on 12/01/16.
+ */
+public class Entry {
+    private String mTitle;
+    private String mText;
+    private String mAuthor;
+    private Integer mSubCount;
+
+    public Entry(String title, String text, String author, Integer subCount) {
+        mTitle = title;
+        mText = text;
+        mAuthor = author;
+        mSubCount = subCount;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        mAuthor = author;
+    }
+
+    public Integer getSubCount() {
+        return mSubCount;
+    }
+
+    public void setSubCount(Integer subCount) {
+        mSubCount = subCount;
+    }
+}

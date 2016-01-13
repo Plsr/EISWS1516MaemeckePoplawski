2fd297e6597cc9084ceccf56202efd7dcbf3a36e
Timo Mämecke <timo@maemecke.com>
2016-01-14 00:49:45 +0100

    Don't send GCM to yourself

--- MS6/service/helpers/GCM.js
+++ MS6/service/helpers/GCM.js
@@ -3,7 +3,11 @@ import { GCM } from "gcm";
 var gcm = new GCM(__config.gcm_api_key);
 
 export function sendGCM(toUser, fromUser, entry) {
+  // Only send if user is registered with gcm in app
   if (!toUser.device_id) return;
+
+  // Stop if user tries to send to himself
+  if (toUser.device_id === fromUser.device_id) return;
 
   let message = {
     registration_id: toUser.device_id,
